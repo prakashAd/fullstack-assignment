b9807decf59c3f5d6fc99be54e440df2c8ef11fa
prakashAd <adhikariprakash992@gmail.com>
2023-07-08 22:09:59 +0545

    user Api added

---
+++ src/api/userApi.js
@@ -0,0 +1,12 @@
+import {API} from "../config"
+
+export const userRegister = (username,email,password) =>{
+    let user = {username,email,password}
+    return fetch(`${API}/register`,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(user)
+})
+}
